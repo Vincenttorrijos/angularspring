e59a1d2f84976bbd86f64f0193216f64c28bc5df
vincent torrijos <vtorrijos18@live.com>
2019-05-16 23:15:53 -0500

    constructed todo's page

--- src/app/component/list-todos/list-todos.component.ts
+++ src/app/component/list-todos/list-todos.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -8,9 +19,9 @@ import { Component, OnInit } from '@angular/core';
 export class ListTodosComponent implements OnInit {
 
   todos = [
-    {id : 1, description : 'Learn to code'},
-    {id : 2, description : 'Learn to code some more'},
-    {id : 3, description : 'Learn to code and more'}
+    new Todo(1, 'learn to code', true, new Date()),
+    new Todo(2, 'learn to code2', false, new Date()),
+    new Todo(3, 'learn to code3', true, new Date())
   ]
 
   constructor() { }
